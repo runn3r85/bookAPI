faf36054b74e511a413328abe003d8a0ff26b55a
Brandon Barrette <brandon.barrette@gmail.com>
2015-05-12 13:01:59 -0700

    add patch and delete methods to API

--- routes/bookRoutes.js
+++ routes/bookRoutes.js
@@ -24,27 +24,59 @@ var routes = function(Book){
       });
     });
 
+  bookRouter.use('/:bookId', function(req, res, next){
+    Book.findById(req.params.bookId, function(err, book){
+      if (err) {
+        res.status(500).send(err);
+      } else if(book) {
+        req.book = book;
+        next();
+      } else {
+        res.status(404).send('no book found');
+      }
+    });
+  });
+
   bookRouter.route('/:bookId')
     .get(function(req, res){
-      Book.findById(req.params.bookId, function(err, book){
+      res.json(req.book);
+    })
+    .put(function(req, res){
+      book = req.book;
+      book.title = req.body.title;
+      book.author = req.body.author;
+      book.genre = req.body.genre;
+      book.read = req.body.read;
+      book.save(function(err){
         if (err) {
           res.status(500).send(err);
         } else {
-          res.json(book);
+          res.json(req.book);
         }
       });
     })
-    .put(function(req, res){
-      Book.findById(req.params.bookId, function(err, book){
+    .patch(function(req, res){
+      if(req.body._id) {
+        delete req.body._id;
+      }
+
+      for(var p in req.body) {
+        req.book[p] = req.body[p];
+      }
+      req.book.save(function(err){
         if (err) {
           res.status(500).send(err);
         } else {
-          book.title = req.body.title;
-          book.author = req.body.author;
-          book.genre = req.body.genre;
-          book.read = req.body.read;
-          book.save();
-          res.json(book);
+          res.json(req.book);
+        }
+      });
+    })
+    .delete(function(req, res){
+      req.book.remove(function(err){
+        if (err) {
+          res.status(500).send(err);
+        } else {
+          res.status(204).send('Removed');
         }
       });
     });
