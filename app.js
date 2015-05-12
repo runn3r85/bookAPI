02794ee6e75040f928e1b74120a723f97d955b1d
Brandon Barrette <brandon.barrette@gmail.com>
2015-05-12 10:02:52 -0700

    add basic get request

--- app.js
+++ app.js
@@ -4,6 +4,16 @@ var app = express();
 
 var port = process.env.PORT || 3000;
 
+var bookRouter = express.Router();
+
+bookRouter.route('/books')
+  .get(function(req, res){
+    var responseJson = { hello: "This is my API" };
+    res.json(responseJson);
+  });
+
+app.use('/api', bookRouter);
+
 app.get('/', function(req, res){
   res.send('Welcome to my API!');
 });
